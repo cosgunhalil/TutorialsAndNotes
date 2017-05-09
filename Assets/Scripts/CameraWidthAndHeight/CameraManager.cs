e88c91d858f979c0b91fa171c0119c5d67e0e1d7
halil cosgun <cosgun.halil@gmail.com>
2017-05-10 00:09:29 +0300

    Tutorials
    -> Pass Multiple Parameters to StartCoroutine
    -> Get Ortographic Camera Width and Height
    -> Ignore layers

---
+++ Assets/Scripts/CameraWidthAndHeight/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    public Camera MainCamera;
+
+	void Start ()
+    {
+        float height = 2f * MainCamera.orthographicSize;
+        float width = height * MainCamera.aspect;
+
+        Debug.Log("Height = " + height + " / Width = " + width);
+    }
+	
+}
---
+++ Assets/Scripts/IgnoreLayers/RaySource.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySource : MonoBehaviour {
+
+    public Transform Target;
+    private Transform _transform;
+    private LayerMask _ignoredLayers;
+
+
+	void Start ()
+    {
+        _transform = GetComponent<Transform>();
+        _ignoredLayers = ~((1 << LayerMask.NameToLayer("Player")) | (1 << (LayerMask.NameToLayer("Friend"))));
+        Shot();
+    }
+
+    private void Shot()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(_transform.position,Target.position, 1000 ,_ignoredLayers);
+
+        if (hit.collider)
+        {
+            Debug.Log("Target: "+ hit.collider.name + " is shotted!");
+        }
+    }
+}
