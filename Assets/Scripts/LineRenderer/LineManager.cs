a55c227706c0abec105082624b942ff78de009fe
halil cosgun <cosgun.halil@gmail.com>
2017-05-10 00:42:00 +0300

    LineRenderer Positions

---
+++ Assets/Scripts/LineRenderer/LineManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineManager : MonoBehaviour {
+
+    public List<Transform> CubeTransforms;
+
+    private LineRenderer _lineRenderer;
+ 
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+
+        DrawMap(CubeTransforms);
+    }
+
+    private void DrawMap(List<Transform> dots)
+    {
+        _lineRenderer.numPositions = dots.Count;
+
+        for (int i = 0; i < dots.Count; i++)
+        {
+            _lineRenderer.SetPosition(i, dots[i].position);
+        }
+    }
+}
