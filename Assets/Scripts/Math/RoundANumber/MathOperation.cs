c415f1191ba17e95ea2219a2f76eb68039985fc0
halil cosgun <cosgun.halil@gmail.com>
2017-05-12 01:36:53 +0300

    Math.Round Example

---
+++ Assets/Scripts/Math/RoundANumber/MathOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class MathOperation : MonoBehaviour {
+
+    public Transform Follower;
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        var mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        Follower.position = RoundCoordinates(mousePosition);
+    }
+
+    public Vector3 RoundCoordinates(Vector3 position)
+    {
+        var roundedPosition = new Vector3(
+                (float)Math.Round(position.x, 1),
+                (float)Math.Round(position.y , 1),
+                0
+            );
+
+        return roundedPosition;
+    }
+}
