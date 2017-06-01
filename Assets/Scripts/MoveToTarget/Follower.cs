3892b34a21bb731c80f58e1ded8bcf427caf7785
Halil Coşgun <cosgun.halil@gmail.com>
2017-06-01 14:40:23 +0300

    move to target tutorial is completed

---
+++ Assets/Scripts/MoveToTarget/Follower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+
+    private float _speed;
+    private Transform _transform;
+
+    public void Init()
+    {
+        _speed = 2f;
+        _transform = GetComponent<Transform>();
+    }
+
+    public void Move(Transform target)
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+		if (distance > 1.0f)
+		{
+            Vector3 dir = target.position - transform.position;
+			dir.Normalize();                                    
+            transform.position += dir * _speed * Time.deltaTime; 
+		}
+	}
+
+}
---
+++ Assets/Scripts/MoveToTarget/MoveToTargetManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTargetManager : MonoBehaviour {
+
+    public List<Follower> Followers;
+    public Transform Target;
+
+    void Start()
+    {
+        InitFollowers();
+    }
+
+    private void InitFollowers()
+    {
+        foreach (var follower in Followers)
+        {
+            follower.Init();
+        }
+    }
+
+    void Update()
+    {
+        foreach (var follower in Followers)
+        {
+            follower.Move(Target);
+        }
+    }
+}
