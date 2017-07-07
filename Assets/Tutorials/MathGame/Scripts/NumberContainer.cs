bb7d1641ab24b7565c084343c3e9374fe62bc4bb
Halil Coşgun <cosgun.halil@gmail.com>
2017-07-07 18:12:33 +0300

    drag and drop

--- Assets/Tutorials/MathGame/Scripts/NumberContainer.cs
+++ Assets/Tutorials/MathGame/Scripts/NumberContainer.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
 
-public class NumberContainer : MonoBehaviour
+public class NumberContainer : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
 
     public Text ValueText;
@@ -36,4 +38,18 @@ public class NumberContainer : MonoBehaviour
         _spriteRenderer.color = color;
     }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        _transform.position = eventData.position;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _transform.SetParent(transform.parent.parent);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _transform.parent = transform.parent.GetChild(0).transform;
+    }
 }
