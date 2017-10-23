4f1f00fb8c76a863fc5977034d02404a6bbfe993
halil cosgun <cosgun.halil@gmail.com>
2017-10-24 00:48:50 +0300

    scriptable objects

---
+++ Assets/Tutorials/ScriptableObjects/Archer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour {
+
+    public ArcherData _archerData;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _archerData.Experience = 1200.9f;
+        _archerData.KilledBossCount = 56;
+        _archerData.SetAge(35);	
+	}
+}
---
+++ Assets/Tutorials/ScriptableObjects/ArcherData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ArcherData : ScriptableObject {
+
+    public float Experience;
+    public float KilledBossCount;
+    [SerializeField]
+    private int _age;
+
+    public void SetAge(int age)
+    {
+        _age = age;
+    }
+
+    public int GetAge()
+    {
+        return _age;
+    }
+}
